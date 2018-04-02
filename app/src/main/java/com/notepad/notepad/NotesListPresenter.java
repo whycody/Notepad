1315f6e1de3f3208722f378b09aa06c70b2c6b00
Oktawian Kausz <play26400@gmail.com>
2018-04-02 12:58:09 +0200

    deleted setNotes() method

--- app/src/main/java/com/notepad/notepad/NotesListPresenter.java
+++ app/src/main/java/com/notepad/notepad/NotesListPresenter.java
@@ -6,13 +6,7 @@ import java.util.List;
 public class NotesListPresenter {
 
     private List<Note> notes = new ArrayList<>();
-
-    public void setNotes(){
-        for(int i =0; i<10; i++){
-            Note note = new Note("Note " + i);
-            notes.add(note);
-        }
-    }
+    
 
     public void onBindRepositoryRowViewAtPosition(int position, NotesRowView rowView) {
        Note note = notes.get(position);
